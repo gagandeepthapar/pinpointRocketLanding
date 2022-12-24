c225d925f7de44bf6c45a36cffe7fd25de2c7ac3
gaganthapar12309 <gsthapar@gmail.com>
2022-12-23 16:15:18 -0800

    Adding files and updating READEME

---
+++ PinPointRocketLanding.m
@@ -0,0 +1,395 @@
+%% Gagandeep Thapar & Anuj Sanghvi
+
+% AERO 560: Final
+% Convex Optimization Implementation:
+        % Convex Optimization for Trajectory Generation: A Tutorial on Generating
+        % Dynamically Feasible Trajectories Reliably and Efficiently
+
+%% Housekeeping
+
+clc;
+close all;
+clear all;
+
+%% Setup
+
+% setup world information (Earth)
+world.x_hat = [1,0,0];
+world.y_hat = [0,1,0];
+world.z_hat = [0,0,1];
+
+world.g = -9.807;
+world.T = 23*3600 + 56*60 + 4.091;  % [s] sidereal time of earth
+world.lat = 34.7420;
+world.t_step = 1;
+
+% create rocket struct (modeled off of Falcon 9/Merlin Engine for Earth landing)
+rocket.g = -3.7114*world.z_hat;
+rocket.omega = (2*pi/world.T)*(world.x_hat*cosd(world.lat) + world.y_hat*0 + world.z_hat*sind(world.lat));
+rocket.m_dry = 1505;
+rocket.m_wet = 1905;
+rocket.isp = 225;
+rocket.phi = 27; % [deg] max cant angle
+rocket.alpha = 1/(rocket.isp*9.807*cosd(rocket.phi)); 
+rocket.T_max = 6 * 3100;
+rocket.thrust_min = 0.3 * rocket.T_max * cosd(rocket.phi);
+rocket.thrust_max = 0.8 * rocket.T_max * cosd(rocket.phi);
+rocket.approach_max = 86;  % [deg]
+rocket.pointing_max = 40;  % [deg]
+rocket.vel_max = 500 * 1000/3600;   % [m/s]
+rocket.pos_initial = (2*world.x_hat' + 0*world.y_hat' + 1.5*world.z_hat')*1000;
+rocket.vel_initial = 80*world.x_hat' + 30*world.y_hat' - 75*world.z_hat';
+
+rocket.w_x = skew(rocket.omega);
+rocket.A_c = [zeros(3,3), eye(3), zeros(3,1);
+                -(rocket.w_x)^2, -2*rocket.w_x, zeros(3,1);
+                zeros(1,7)];
+rocket.B_c = [zeros(3,4);
+                eye(3), zeros(3,1);
+                zeros(1,3), -rocket.alpha];
+rocket.p_c = [zeros(3,1);rocket.g';0];
+[rocket.n, rocket.m] = size(rocket.B_c);
+tFlight = (rocket.m_wet - rocket.m_dry)/(rocket.alpha*rocket.thrust_min);
+
+%% PDG FFT
+
+delt = 1;
+tFlight = round(tFlight);
+t = 0:delt:tFlight;
+
+iters = (tFlight)/delt + 1;
+
+r = zeros(iters,3);
+v = zeros(iters,3);
+m = zeros(iters,1);
+
+r(1,:) = rocket.pos_initial;
+v(1,:) = rocket.vel_initial;
+m(1) = rocket.m_wet;
+
+
+s_r = zeros(1,3);
+S_r = diag([max(1, abs(r(1,1))), max(1, abs(r(1,2))), max(1, abs(r(1,3)))]);
+s_v = zeros(1,3);
+S_v = diag([max(1, abs(v(1,1))), max(1, abs(v(1,2))), max(1, abs(v(1,3)))]);
+s_z = (log(rocket.m_dry) + log(rocket.m_wet))/2;
+S_z = log(rocket.m_wet)-s_z;
+s_u = [0,0,0.5*(rocket.thrust_min/rocket.m_wet*cosd(rocket.pointing_max)+rocket.thrust_max/rocket.m_dry)];
+S_u = diag([rocket.thrust_max/rocket.m_dry*sind(rocket.pointing_max), ...
+            rocket.thrust_max/rocket.m_dry*sind(rocket.pointing_max), ...
+            rocket.thrust_max/rocket.m_dry-s_u(3)]);
+s_zeta = s_u(3);
+S_zeta = S_u(3,3);
+
+H_gs = [cosd(rocket.approach_max), 0, -sind(rocket.approach_max);
+        -cosd(rocket.approach_max), 0, -sind(rocket.approach_max);
+        0, cosd(rocket.approach_max), -sind(rocket.approach_max);
+        0, -cosd(rocket.approach_max), -sind(rocket.approach_max)];
+
+h_gs = zeros(4,1);
+
+% PDG setup
+pdg = optimproblem;
+r_s = optimvar('r_s', 3, iters);
+v_s = optimvar('v_s', 3, iters);
+z_s = optimvar('z_s', 1, iters);
+u_s = optimvar('u_s', 3, iters-1);
+zeta_s = optimvar('zeta_s', 1, iters-1);
+
+% Scaling
+r = (S_r*r_s) + repmat(s_r, iters,1)';
+v = (S_v*v_s) + repmat(s_v, iters,1)';
+z = (S_z*z_s) + repmat(s_z, iters,1)';
+u = (S_u*u_s) + repmat(s_u, iters-1,1)';
+zeta = (S_zeta*zeta_s) + repmat(s_zeta, iters-1,1)';
+
+% initial and final constraints
+r0 = optimconstr(1);
+v0 = optimconstr(2);
+z0 = optimconstr(3);
+
+rF = optimconstr(4);
+vF = optimconstr(5);
+zF = optimconstr(6);
+
+r0 = r(:,1) == rocket.pos_initial;
+rF = r(:,end) == zeros(3,1);
+v0 = v(:,1) == rocket.vel_initial;
+vF = v(:,end) == zeros(3,1);
+z0 = z(1) == log(rocket.m_wet);
+zF = z(end) >= log(rocket.m_dry);
+
+pdg.Constraints.r0 = r0;
+pdg.Constraints.v0 = v0;
+pdg.Constraints.z0 = z0;
+pdg.Constraints.rF = rF;
+pdg.Constraints.vF = vF;
+pdg.Constraints.zF = zF;
+
+dynamicConstraint = optimconstr(iters-1,7);
+thrustMinConstraint = optimconstr(iters-1, 1);
+thrustMaxConstraint = optimconstr(iters-1, 1);
+massMinConstraint = optimconstr(iters, 1);
+massMaxConstraint = optimconstr(iters, 1);
+glideslopeConstraint = optimconstr(iters, 4);
+attitudeConstraint = optimconstr(iters-1,3);
+% maxVelConstraint = optimconstr(iters, 1);
+% subsurfaceConstraint = optimconstr(iters-1,1);
+
+[A,B,p] = discretize(rocket, delt);
+
+% time-based constraints
+for i = 1:iters-1
+
+    % dynamic constraints
+    dynamicConstraint(i,1:3) = r(:,i+1)    == r(:,i) + (v(:,i))*delt;
+    dynamicConstraint(i,4:6) = v(:,i+1)    == v(:,i) + ((zeta(i)*u(:,i)/z(i)) + rocket.g')*delt;
+    dynamicConstraint(i,7)   = z(i+1)      == (z(i)) - log(rocket.alpha*zeta(i)*delt);
+
+%     subsurfaceConstraint(i,:) = r(3,i) >= 0;
+
+    % thrust
+    zNOT = log(rocket.m_wet - rocket.alpha*rocket.thrust_max*t(i));
+    uMin = rocket.thrust_min*exp(-zNOT);
+    uMax = rocket.thrust_max*exp(-zNOT);
+    sigZ = z(i) - zNOT;
+
+    thrustMinConstraint(i) = zeta(i) >= uMin*(1-sigZ + 0.5*sigZ^2);
+    thrustMaxConstraint(i) = zeta(i) <= uMax*(1-sigZ);
+
+    % mass
+    massMinConstraint(i) = zNOT <= z(i);
+    massMaxConstraint(i) = z(i) <= log(rocket.m_wet - rocket.alpha*rocket.thrust_min*t(i));
+
+%     glideslope
+    glideslopeConstraint(i,:) = H_gs*(r(:,i)) <= h_gs;
+
+    % attitude
+    attitudeConstraint(i,:) = dot(u(:,i), [0;0;1]) >= zeta(i)*cosd(rocket.pointing_max);
+
+end
+
+%     velmax
+%     maxVelConstraint(i) = norm(v(:,i)) <= rocket.vel_max;ﬁ
+
+% final state constraints
+% mass
+    zNOT = log(rocket.m_wet - rocket.alpha*rocket.thrust_max*t(iters));
+    massMinConstraint(end) = zNOT <= z(end);
+    massMaxConstraint(end) = z(end) <= log(rocket.m_wet - rocket.alpha*rocket.thrust_min*t(end));
+
+% glideslope
+    glideslopeConstraint(end,:) = H_gs*(r(:,end)) <= h_gs;
+
+% velocity
+%     maxVelConstraint(end) = norm(v(:,end)) <= rocket.vel_max;
+
+disp('post')
+
+pdg.Constraints.dynamicConstraint = dynamicConstraint;
+pdg.Constraints.thrustMinConstraint = thrustMinConstraint;
+pdg.Constraints.thrustMaxConstraint = thrustMaxConstraint;
+pdg.Constraints.massMinConstraint = massMinConstraint;
+pdg.Constraints.massMaxConstraint = massMaxConstraint;
+pdg.Constraints.attitudeConstraint = attitudeConstraint;
+pdg.Constraints.glideslopeConstraint = glideslopeConstraint;
+% pdg.Constraints.subsurfaceConstraint = subsurfaceConstraint;
+% pdg.Constraints.maxVelConstraint = maxVelConstraint;
+
+pdg.Objective = sum(zeta);
+
+%%
+
+x0.r_s = zeros(3, iters);
+x0.u_s = zeros(3, iters-1);
+x0.v_s = zeros(3, iters);
+x0.z_s = zeros(1, iters);
+x0.zeta_s = zeros(1, iters-1);
+
+x0.r_s(:,1) = ones(3,1);
+x0.v_s(:,1) = [1;1;-1];
+x0.u_s(:,1) = zeros(3,1);
+x0.z_s(1) = log(rocket.m_wet);
+x0.zeta_s(1) = 0;
+
+%%
+solveFlag = 0;
+
+tic
+if solveFlag == 1
+
+    disp('Solving problem with fmincon')
+    
+    options = optimoptions('fmincon', 'ConstraintTolerance',1e-3, 'OptimalityTolerance',1e-3, 'EnableFeasibilityMode',true, 'SubproblemAlgorithm','cg', 'Display', 'off');
+    x = solve(pdg, x0, solver='fmincon');
+
+else
+
+    fprintf('Loading in data from mat file\n\n');
+    load('subsurface_data.mat');
+
+end
+toc
+
+% solution = [r, v, z, u, zeta, m];
+
+%% Plots
+close all
+
+scaledr = S_r*x.r_s;
+
+figure
+subplot(3,1,1)
+plot(t, scaledr(1,:))
+ylabel('X [m]')
+
+subplot(3,1,2)
+plot(t, scaledr(2,:))
+ylabel('Y [m]')
+
+subplot(3,1,3)
+plot(t, scaledr(3,:))
+ylabel('Z [m]')
+xlabel('Time [sec]')
+
+sgtitle('Landing Trajectory')
+
+figure
+hold on
+plot(scaledr(1,:),scaledr(3,:))
+scatter(scaledr(1,1), scaledr(3,1), 'rx')
+scatter(scaledr(1, end), scaledr(3,end), 'kx')
+hold off
+title('Downrange Trajectory')
+legend('Trajectory', 'Starting Point', 'Landing Point')
+xlabel('Distance from Landing Point [m]')
+ylabel('Altitude [m]')
+
+figure
+hold on
+plot(scaledr(2,:), scaledr(3,:))
+scatter(scaledr(2,1), scaledr(3,1), 'rx')
+scatter(scaledr(2, end), scaledr(3,end), 'kx')
+hold off
+title('Crossrange Trajectory')
+xlabel('Crossrange [m]')
+ylabel('Altitude [m]')
+legend('Trajectory', 'Starting Point', 'Landing Point')
+
+figure
+plot3(scaledr(1,:), scaledr(2,:), scaledr(3,:))
+xlabel('X [m]')
+ylabel('Y [m]')
+zlabel('Z [m]')
+title('3D Landing Trajectory')
+
+vnorm = vecnorm(S_v*x.v_s,2,1);
+
+figure
+plot(t, vnorm)
+title('Rocket Norm of Velocity')
+xlabel('Time [sec]')
+ylabel('Velocity [m/s]')
+
+
+mass =  rocket.m_dry*exp(S_z*x.z_s);
+figure
+hold on
+plot([t(1),t(end)], [rocket.m_dry, rocket.m_dry], 'r--')
+plot(t, mass, 'b')
+hold off
+title('Mass of Rocket')
+legend('Rocket Dry Mass')
+xlabel('Time [sec]')
+ylabel('Mass [kg]')
+
+%% Functions
+
+function wx = skew(w)
+
+    wx = [0, -w(3) w(2);
+            w(3) 0 -w(1);
+            -w(2) w(1) 0];
+
+end
+
+function [A,B,p] = discretize(rocket, delT)
+
+    Ar = rocket.A_c;
+    Br = rocket.B_c;
+    pr = rocket.p_c;
+    
+    matr = [Ar, Br, pr;
+            zeros(rocket.m+1, rocket.n+rocket.m+1)] * delT;
+    
+    M = exp(matr);
+
+    A = M(1:rocket.n, 1:rocket.n);
+    B = M(1:rocket.n, rocket.n+1:rocket.m+rocket.n);
+    p = M(1:rocket.n, rocket.n+rocket.m+1);
+
+end
+
+function solution = simulate(rocket, control, tFlight)
+
+dynamics = @(t,x) rocket.A_c*x + rocket.B_c*control(t, x, rocket) + rocket.p_c;
+x0 = [rocket.pos_initial;rocket.vel_initial;log(rocket.m_wet)];
+t_step = 0.01;
+
+tspan = 0:t_step:tFlight;
+[time, dstate] = ode45(@dynamics, tspan, x0);
+
+U = [];
+for i = 1:length(time)
+    U = [U, control(t(i), dstate(i,:), rocket)];
+end
+
+N = length(time);
+
+% save solution
+
+r = dstate(:,1:3);
+v = dstate(:,4:6);
+z = dstate(:,7);
+u = U(:,1:3);
+zeta = U(:,4);
+
+m = exp.(z);
+
+T = [];
+for i = 1:3
+    T = [T, m.*u(:,i)];
+end
+T = T';
+
+T_nrm = zeros(N,1);
+for i = 1:N
+   T_nrm(i) = norm(T(i,:));
+end
+
+gamma = zeros(N,1);
+
+for i = 1:N
+    gamma(i) = acosd(dot(T(i,:), [0;0;1])/(norm(T(i,:))));
+end
+
+solution.time = t;
+solution.pos = r;
+solution.vel = v;
+solution.log_mass = z;
+solution.accel = u;
+solution.accel_mag = zeta;
+solution.cost = 0;
+solution.Thrust_traj = T;
+solution.Thrust_nrm = T_nrm;
+solution.mass = m;
+solution.point = gamma;
+
+end
+
+function solution = solve_pdg_fft(rocket, tFlight, delt)
+
+
+end
+
+
--- README.md
+++ README.md
@@ -1,2 +1,20 @@
-# pinpointRocketLanding
-Simulation of a trajectory for pinpoint Rocket Landing
+# Pinpoint Rocket Landing
+
+## Background
+Trajectory generation of an arbitrary rocket following the path laid out in *Convex Optimization for Trajectory Optimization: A Tutorial on Generating Dynamically Feasible Trajectories Reliably and Efficiently*, D. Malyuta et al.
+
+Several suboptimal solutions were found (data can be found in **mat_files** directory). While the focus of the paper was Convex Optimization and Second Order Cone Programming, MATLAB is more suited to use *fmincon*, another approach to minimizing functions. *fmincon* is substantially slower than other SOCP solvers and, consequentially, takes ~30-45min to generate a 150 second trajectory given the constraints.
+ 
+Dynamics of the rocket were encoded as individual constraints at each time step constrained by the amount of propellant onboard the vehicle. A standard state propagator was used, dissimilar to the approach in the paper, to define how the state changes over time.
+
+Some interesting trajectories were generated such as a subsurface flight (violation of the glideslope constraints)
+![subsurface_2d](media/subsurface_2dlanding.png)
+![subsurface_3d](media/subsurface_3dlanding.png)
+![subsurface_dims](media/subsurface_dimensionlanding.png)
+
+and a technically feasible trajectory that approaches the ground vertically instead of following the glideslope closer
+![feasible_2d](media/2dlanding.png)
+![feasible_3d](media/3dlanding.png)
+![feasible_dim](media/dimensionlanding.png)
+
+Future work includes modifying constraints and possibly porting the code to a lighter language e.g., Python to improve performance. It should be noted that the SOCP solver and constraint handler used in the paper, [ECOS](https://github.com/embotech/ecos) and [JuMP](https://github.com/jump-dev/JuMP.jl) respectively, were not used as there was no obvious support for MATLAB. ECOS has better Python support, although JuMP appears to be [Julia](https://julialang.org/) specific.
